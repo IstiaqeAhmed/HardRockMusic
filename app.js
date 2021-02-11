97eb2d8a77d2486ea435d5e57f31e6154703d3e3
Istiaqe Ahmed Khan <istiaqeahmed.cse@gmail.com>
2021-02-11 12:46:32 +0600

    error handling

--- app.js
+++ app.js
@@ -5,12 +5,12 @@ const searchSongs = () => {
    fetch(url)
    .then(res => res.json())
    .then(data => displaySongs(data.data))
+   .catch(error => displayError('Something Went Wrong!! Please try again letter!!'));
 }
 const displaySongs = songs => {
     const songContainer = document.getElementById('song-container');
     songContainer.innerHTML = ''
       songs.forEach(song =>{
-          console.log(song)
          const songDiv = document.createElement('div');
          songDiv.className = 'single-result row align-items-center my-3 p-3'
         songDiv.innerHTML = 
@@ -40,4 +40,8 @@ const getLyric = (artist, title) => {
 const displayLyrics = lyrics => {
     const lyricsDiv = document.getElementById('song-lyrics')
     lyricsDiv.innerText = lyrics;
+}
+const displayError = error => {
+    const errorTag = document.getElementById('error-message');
+    errorTag.innerText = error;
 }
